66f24f8a8b27d7e5feac08719b334e610fdee029
admin <admin@admins-MacBook-Pro.local>
2018-05-26 20:00:01 -0700

    sable web design page v.1.0

--- src/index-page.js
+++ src/index-page.js
@@ -1,30 +1,4 @@
 window.sr = ScrollReveal();
 sr.reveal('.foo');
-let formButton = document.getElementById('form_input_button');
-let email = document.getElementById('email_input');
-let textMain = document.getElementById('text_input');
 
-class handleMessage {
-    constructor(email = 'Empty Email Field', subject = "Empty Subject Field", text = 'Empty Text Field') {
-        this.email = email;
-        this.text = text;
-        this.subject = subject;
-        let tempMessage = {
-            email: this.email,
-            content: {
-                subject: this.subject,
-                text: this.text
-            }
-        };
-        return tempMessage;
-    }
-}
-;
-
-
-formButton.onclick = () => {
-
-    alert('test')
-
-}
 
